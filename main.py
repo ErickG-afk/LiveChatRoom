422683a35e2fd52e6ee982fcafbe4d3881ef85ec
ErickGPc <garciaerickw@gmail.com>
2023-03-14 17:26:46 +0100

    Styling the chatbox Creating socket functions to send messages Display room name

--- main.py
+++ main.py
@@ -56,7 +56,60 @@ def home():
 #RoomPage
 @app.route("/room")
 def room():
-    return render_template("room.html")
+    room = session.get("room")
+
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+
+    return render_template("room.html", code=room, messages=rooms[room]["messages"])
+
+#Function to enable the server to transmit the messages to all users inside the room
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms: return
+
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+    send(content, to= room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
+
+
+#Working with flask sockets, in this to send message to a room
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+
+    if not name or not room:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    send({"name": name, "message": "Has entered the room"}, to=room)
+    rooms[room]["members"] +=1 
+    print(f"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+
+    leave_room(room)
+
+    if room in rooms:
+           rooms[room]["members"] -=1
+           if rooms[room]["members"] <=0:
+               del rooms[room]
+        
+    send({"name": name, "message": "Has left the room"}, to=room)
+    print(f"{name} left room {room}")
+
 
 #Debug True in order to automatically refresh the changes onto the web page
 if __name__== "__main__":
