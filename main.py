25b1bbdc0b7ea9a04107d41d277b06483881f136
ErickGPc <garciaerickw@gmail.com>
2023-03-14 09:53:46 +0100

    creating project of chat room

---
+++ main.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template,request,session,redirect,url_for
+from flask_socketio import join_room, leave_room,send,SocketIO
+import random
+from string import ascii_uppercase
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] ="ergtbvsdfasfdv"
+socketio = SocketIO(app)
+
+rooms={}
+
+#Creating function to generate a room code 
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+
+        if code not in rooms:
+            break
+    
+    return code
+
+#Creating roots for the website
+#Home Page
+@app.route("/", methods=["POST","GET"])
+def home():
+
+    session.clear()
+    if request.method == "POST":
+        name = request.form.get("name")
+        code =request.form.get("code")
+        join = request.form.get("join",False)
+        create = request.form.get("create",False)
+
+        if not name:
+             return render_template("home.html", error="Please enter a username.", code = code, name = name)
+        
+        if join != False and not code:
+            return render_template("home.html", error="Please enter a room code before attempting to join.",code = code, name = name)
+        
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members" : 0, "messages": []}
+        elif code not in rooms:
+             return render_template("home.html", error="Please enter valid room code, room does not exist.",code = code, name = name)
+
+        session["room"] = room
+        session["name"] = name
+
+        return redirect(url_for("room"))
+
+    return render_template("home.html")
+
+#RoomPage
+@app.route("/room")
+def room():
+    return render_template("room.html")
+
+#Debug True in order to automatically refresh the changes onto the web page
+if __name__== "__main__":
+    socketio.run(app, debug= True)
